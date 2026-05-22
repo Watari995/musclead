f8bf06a387863e101d4ac8e7c45c02bdfa56f52c
Wataru Ichimura <wata_2@icloud.com>
2026-05-23 01:45:34 +0900

    feat(myerror): define ErrorCode types and ErrorCodes catalog
    - ErrorCode 型 (string ベース)
    - 階層構造: General / User / Meal / Training / Weight
    - 各カテゴリのサブ struct (generalErrors / userErrors 等)
    - ErrorCodes 変数で IDE 補完が効くアクセス
      例: ErrorCodes.User.EmailAlreadyExistsError
    - コード文字列は snake_case で統一 (FE / ログ集計で扱いやすく)
    
    設計方針 (musclead での適用):
    - 汎用エラー (formバリデーション等) は General で十分
    - FE が UI 分岐するドメイン固有エラーのみ Code 化
    - パスワード違いは InvalidCredentialsError でぼかし (セキュリティ)

---
+++ server/internal/myerror/error_codes.go
@@ -0,0 +1,67 @@
+package myerror
+
+type ErrorCode string
+
+type generalErrors struct {
+	InternalError        ErrorCode
+	ValidationError      ErrorCode
+	PermissionError      ErrorCode
+	BadRequestError      ErrorCode
+	IncompatibleError    ErrorCode
+	NotFoundError        ErrorCode
+	UnauthorizedError    ErrorCode
+	MaintenanceModeError ErrorCode
+	DisabledUserError    ErrorCode
+}
+
+type userErrors struct {
+	NotFoundError           ErrorCode
+	EmailAlreadyExistsError ErrorCode
+	InvalidCredentialsError ErrorCode
+}
+
+type mealErrors struct {
+	NotFoundError ErrorCode
+}
+
+type trainingErrors struct {
+	NotFoundError ErrorCode
+}
+
+type weightErrors struct {
+	NotFoundError ErrorCode
+}
+
+var ErrorCodes = struct {
+	General  generalErrors
+	User     userErrors
+	Meal     mealErrors
+	Training trainingErrors
+	Weight   weightErrors
+}{
+	General: generalErrors{
+		InternalError:        "general.internal_error",
+		ValidationError:      "general.validation_error",
+		PermissionError:      "general.permission_error",
+		BadRequestError:      "general.bad_request_error",
+		IncompatibleError:    "general.incompatible_error",
+		NotFoundError:        "general.not_found_error",
+		UnauthorizedError:    "general.unauthorized_error",
+		MaintenanceModeError: "general.maintenance_mode_error",
+		DisabledUserError:    "general.disabled_user_error",
+	},
+	User: userErrors{
+		NotFoundError:           "user.not_found_error",
+		EmailAlreadyExistsError: "user.email_already_exists_error",
+		InvalidCredentialsError: "user.invalid_credentials_error",
+	},
+	Meal: mealErrors{
+		NotFoundError: "meal.not_found_error",
+	},
+	Training: trainingErrors{
+		NotFoundError: "training.not_found_error",
+	},
+	Weight: weightErrors{
+		NotFoundError: "weight.not_found_error",
+	},
+}
