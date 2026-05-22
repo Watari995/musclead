2e6e1e3187575fdb0f3c9d9fa06ab3109278e255
Wataru Ichimura <wata_2@icloud.com>
2026-05-22 17:26:42 +0900

    feat(meal): scaffold meal domain layer
    - MealID / MealPhotoID
    - Meal / MealPhoto / Repository ファイル枠
    - internal/ で境界強制

---
+++ server/internal/meal/internal/domain/meal_id.go
@@ -0,0 +1,7 @@
+package mealdomain
+
+import "github.com/Watari995/musclead/internal/valueobject"
+
+type MealID struct {
+	valueobject.PrimaryIdBase
+}
---
+++ server/internal/meal/internal/domain/meal_photo_id.go
@@ -0,0 +1,7 @@
+package mealdomain
+
+import "github.com/Watari995/musclead/internal/valueobject"
+
+type MealPhotoID struct {
+	valueobject.PrimaryIdBase
+}
