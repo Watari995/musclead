5f2349d6d58c147800522a943f764d2f3727c998
Wataru Ichimura <wata_2@icloud.com>
2026-05-24 18:44:13 +0900

    feat(valueobject): implement NonNegativeDecimal validation
    - LessThan(decimal.Zero) で 0未満を弾く (0以上 OK)
    - WeightKg / Percentage と同じ validation.By パターン
    - Meal の P/F/C (DECIMAL(6,2)) で使用予定

--- server/internal/valueobject/non_negative_decimal.go
+++ server/internal/valueobject/non_negative_decimal.go
@@ -1,5 +1,35 @@
 package valueobject
 
+import (
+	"errors"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/shopspring/decimal"
+)
+
 type NonNegativeDecimal struct {
 	DecimalBase
+}
+
+func validateNonNegativeDecimal(s any) error {
+	d, ok := s.(decimal.Decimal)
+	if !ok {
+		return errors.New("must be a valid decimal")
+	}
+	if d.LessThan(decimal.Zero) {
+		return errors.New("must be greater than or equal to 0")
+	}
+	return nil
+}
+
+func (n NonNegativeDecimal) Validate() error {
+	return validation.Validate(n.Value(), validation.By(validateNonNegativeDecimal))
+}
+
+func NewNonNegativeDecimal(v decimal.Decimal) (*NonNegativeDecimal, error) {
+	n := NonNegativeDecimal{DecimalBase: DecimalBase{v: v}}
+	if err := n.Validate(); err != nil {
+		return nil, err
+	}
+	return &n, nil
 }
